f73799252b2d4fcb805ec492b2f11a4e2bb30b17
jschwel1 <jschwel1@github.com>
2017-11-06 16:04:52 -0500

    RWM: It works!

--- schwell-j-RWM/schwell-j-RWM.cpp
+++ schwell-j-RWM/schwell-j-RWM.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,29 +7,32 @@
 
 #ifdef WIN32
 #include <time_functions.h>
+#define HOME_DIR ""
+#define FILE_PATH "C:\\Windows\\temp\\courseout\\p4-out.txt"
 #else
 #include "time_functions.h"
+#define HOME_DIR getenv("HOME")
+#define FILE_PATH "/temp/courseout/p4-out.txt"
 #endif
 
 #include <pthread.h>
 #include <semaphore.h>
 
-#ifdef DEBUGGING
-#define DEBUG(...) (__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
-
 #define ROUNDS 10 
 
+/** Monitor Class:
+	This class defines the monitor and all its required functions to 
+	allow synchronization and mutual exclusion for readers and writers.
+**/
 class Monitor{
-
 private:
-	int numReading, numWaitingRs;
-	int numWriting, numWaitingWs;
+	int numReaders;
+	int waitingRs;
+	int writers;
 	pthread_cond_t canRead = PTHREAD_COND_INITIALIZER;
 	pthread_cond_t canWrite = PTHREAD_COND_INITIALIZER;
-	pthread_mutex_t monitorMutex = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t writeLock = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t fileIOLock = PTHREAD_MUTEX_INITIALIZER;
 	std::string data;
 
 	std::string timeToString(){
@@ -42,10 +46,9 @@ private:
 public:
 	Monitor()
 	{
-	    numReading = 0;
-		numWaitingRs = 0;
-		numWriting = 0;
-		numWaitingWs = 0;
+	    numReaders = 0;
+		waitingRs = 0;
+		writers = 0;
 		data = timeToString();
 	}
 
@@ -58,77 +61,74 @@ public:
 		return data;
 	}
 
-	void beginRead(int i){
-		pthread_mutex_lock(&monitorMutex);
+	void beginRead(){
+		pthread_mutex_lock(&writeLock);
 		// If any writers are currently writing or waiting to write, do not enter
-		while ((numWriting > 0) || (numWaitingWs > 0)){
-			numWaitingRs++;
-			//std::cout << "Num Waiting Ws: " << numWaitingWs << "\t Currently Writing: " << numWriting << std::endl;
-			std::cout << "(Rb) Writing: " << numWriting << "\tWs Waiting: " << numWaitingWs << "\tnumReading: " << numReading << "\t waiting Rs " << numWaitingRs << std::endl;
-			pthread_cond_wait(&canRead, &monitorMutex);
-			numWaitingRs--;
+		while (writers > 0){
+			waitingRs++;
+			pthread_cond_wait(&canRead, &writeLock);
+			waitingRs--;
 		}
 		// Increment the number of readers, let other readers know they can enter,
 		// then release the lock.
-		numReading++;
-		std::cout << "-------------- reader " << i << " entered ------" << std::endl;
-		/******** OUTPUT TO FILE *********/
+		numReaders++;
 		pthread_cond_signal(&canRead);
-		pthread_mutex_unlock(&monitorMutex);
+		pthread_mutex_unlock(&writeLock);
+		pthread_mutex_lock(&fileIOLock);
 	}
 
-	void endRead(int i){
-		pthread_mutex_lock(&monitorMutex);
-		numReading--;
+	void endRead(){
+		pthread_mutex_unlock(&fileIOLock);
+		pthread_mutex_lock(&writeLock);
+		numReaders--;
 		// Begin reading signals additional readers to enter if no writers are waiting
 		// so endRead only needs to signal writers (once all readers finish)
-		if (numReading == 0){
+		if (numReaders == 0){
 			pthread_cond_signal(&canWrite);
-			std::cout << " --- signaled canWrite *" << numWaitingWs << " waitingWs, " << numWaitingRs << " waitingRs" << std::endl;
 		}
-		std::cout << "-------------- reader " << i << " done ------" << std::endl;
-		pthread_mutex_unlock(&monitorMutex);
+		pthread_mutex_unlock(&writeLock);
 	}
 
-	void beginWrite(int i){
-		pthread_mutex_lock(&monitorMutex);
+	void beginWrite(){
+		pthread_mutex_lock(&writeLock);
 		// If any writers are writing or any readers are reading
-		while ((numWriting > 0) || (numReading > 0)){
-			numWaitingWs++;
-			std::cout << "(Wb) Writing: " << numWriting << "\tWs Waiting: " << numWaitingWs << "\tnumReading: " << numReading << "\t waiting Rs " << numWaitingRs << std::endl;
-			pthread_cond_wait(&canWrite, &monitorMutex);
-			numWaitingWs--;
+		while (numReaders > 0){
+			pthread_cond_wait(&canWrite, &writeLock);
 		}
-		numWriting++;
-		std::cout << "-------------- writer " << i << " entered ------" << std::endl;
-		pthread_mutex_unlock(&monitorMutex);
+		writers++;
 	} 
-	void endWrite(int i){
-		pthread_mutex_lock(&monitorMutex);
-		numWriting--;
+	void endWrite(){
+		writers--;
 		// Since readers waiting to enter prevent additional readers from entering
-		std::cout << "-------------- writer " << i << " done ------";
-		if (numWaitingRs > 0){
+		if (waitingRs > 0){
 			pthread_cond_signal(&canRead);
-			std::cout << " --- signaled canRead *" << numWaitingWs << " waitingWs, " << numWaitingRs << " waitingRs" << std::endl;
 		}
 		else{
 			pthread_cond_signal(&canWrite);
-			std::cout << " --- signaled canWrite *" << numWaitingWs << "waitingWs,  " << numWaitingRs << " waitingRs" << std::endl;
 		}
-		pthread_mutex_unlock(&monitorMutex);
+		pthread_mutex_unlock(&writeLock);
 	}
 };
 
+/** argType
+	This is the struct passed to threads as a parameter
+**/
 struct argType{
 	Monitor* monitor;
 	int threadNum;
 	int delay;
+	std::fstream* fs;
 };
 
+// Thread function declarations
 void* readThreadStart(void* arg);
 void* writeThreadStart(void* arg);
 
+/** Main function
+	This function obtains the number of threads and their delays
+	from the user and starts the corresponding threads. Once all 
+	the threads complete, it joins them back up and returns.
+**/
 int main(int argc, char** argv){
 	int numReaders, numWriters, readDelay, writeDelay;
 	pthread_t* threads;
@@ -153,6 +153,19 @@ int main(int argc, char** argv){
 		writeDelay = atoi(argv[4]);
 	}
 
+	#ifdef WIN32
+	std::string filePath = FILE_PATH;
+	#else
+	std::string filePath = getenv("HOME");
+	filePath+=FILE_PATH;
+	#endif
+	std::fstream fs;
+	fs.open(filePath.c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
+	if (!fs.is_open()){
+		std::cout << "ERROR: Could not open file " << FILE_PATH << std::endl;
+		return 1;
+	}
+	// Get the space for the threads and their arguments
 	threads = (pthread_t*)malloc(sizeof(pthread_t)*(numReaders+numWriters));
 	threadArgs = (argType*)malloc(sizeof(argType)*(numReaders+numWriters));
 	
@@ -163,6 +176,7 @@ int main(int argc, char** argv){
 										 : i - numReaders;
 		threadArgs[i].delay = (i < numReaders) ? readDelay
 								     : writeDelay;
+		threadArgs[i].fs = &fs;
 		pthread_create(&threads[i], NULL, ((i < numReaders)?readThreadStart
 														   :writeThreadStart), (void*)&threadArgs[i]);
 	}
@@ -170,6 +184,7 @@ int main(int argc, char** argv){
 		pthread_join(threads[i], NULL);
 	}
 
+	fs.close();
 	free(threads);
 	free(threadArgs);
 	std::cout << "Hit <enter> to continue";
@@ -182,11 +197,9 @@ void* readThreadStart(void* arg){
 	Monitor* monitor = a->monitor;
 	int i;
 	for (i = 0; i < ROUNDS; i++){
-//		DEBUG(std::cout << "Reader " << a->threadNum << " beginRead() -- round " << i << std::endl);
-		monitor->beginRead(a->threadNum);
-		std::cout << ">>> DB value read = " << monitor->getDBValue() << " by reader number " << a->threadNum << std::endl;
-//		DEBUG(std::cout << "Reader " << a->threadNum << " endRead() -- round " << i << std::endl);
-		monitor->endRead(a->threadNum);
+		monitor->beginRead();
+		*(a->fs) << ">>> DB value read = " << monitor->getDBValue() << " by reader number " << a->threadNum << std::endl;
+		monitor->endRead();
 		millisleep(a->delay);
 	}
 	return NULL;
@@ -197,11 +210,9 @@ void* writeThreadStart(void* arg){
 	Monitor* monitor = a->monitor;
 	int i;
 	for (i = 0; i < ROUNDS; i++){
-		monitor->beginWrite(a->threadNum);
-//		DEBUG(std::cout << "Writer " << a->threadNum << " beginWrite() -- round " << i << std::endl);
-		std::cout << "*** DB value set to " << monitor->updateDBValue() << " by writer number " << a->threadNum << std::endl;
-//		DEBUG(std::cout << "Writer " << a->threadNum << " endWrite() -- round " << i << std::endl);
-		monitor->endWrite(a->threadNum);
+		monitor->beginWrite();
+		*(a->fs) << "*** DB value set to " << monitor->updateDBValue() << " by writer number " << a->threadNum << std::endl;
+		monitor->endWrite();
 		millisleep(a->delay);
 	}
 	return NULL;
